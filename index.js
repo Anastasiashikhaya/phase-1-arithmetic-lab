423860bfd7f0b0acb932bd74bec11d6d4d9dc058
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-03-20 19:50:33 +0300

    anna's changes

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+// Declare num 1 and num 2
+let num1 = 2;
+let num2 = 31;
+// Multiply num1 and num2, result should be 62
+let multiply = num1 * num2; //2 * 31 = 62
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; //Ensure a value > 0
+//Declare num3 and num4 for modulus operation
+let num3 = 16;
+let num4 = 6;
+// Calculate the remainder of dividing num3 by num4 //should be 4
+let mod = num3 % num4; //16 % 6 = 4 
+//find te highest number in the set, should be 20
+let max = Math.max(1, 5, 10, 15, 20); // Returns 20
